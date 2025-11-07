302d939b3d56cc8a8953720ed49ec703abe28538
Ani861 <gkaniruddha26@gmail.com>
2025-11-07 10:13:03 +0530

    Update etl.py

--- etl.py
+++ etl.py
@@ -8,14 +8,17 @@ import pandas as pd
 import mysql.connector
 from datetime import datetime, timezone
 from decimal import Decimal
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
 DB_CONFIG = {
-    "host": "localhost",
-    "port": 3306,
-    "user": "root",
-    "password": "password",     
-    "database": "etl"           #
+    "host": os.getenv("DB_HOST"),
+    "port": int(os.getenv("DB_PORT")),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "database": os.getenv("DB_NAME")
 }
 
 
